ad78511f8b86bb5e54f4f553770a24e4e83b02ba
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-03-21 12:14:32 +0800

    🐛 Fix: Hidden Switch rheme in player video

--- src/components/bar/SideBar.tsx
+++ src/components/bar/SideBar.tsx
@@ -29,24 +29,28 @@ export function SideBar() {
 
   const menuItens = [
     {
+      id: 'home',
       path: '/',
       name: 'Inicio',
       icon: mdiHomeVariant
     },
     {
-        path: '/for-you',
-        name: 'Para Você',
-        icon: mdiHeart
+      id: 'for-you',
+      path: '/for-you',
+      name: 'Para Você',
+      icon: mdiHeart
     }
   ]
 
   const sideItens = [
     {
+        id: 'theme-switch',
         name: 'Theme Switch',
         icon: mdiWhiteBalanceSunny,
         function: handleThemeSwitcher
     },
     {
+      id: 'settings',
       path: '/settings',
       name: 'Configurações',
       icon: mdiCog
@@ -55,52 +59,54 @@ export function SideBar() {
 
   return (
     <motion.div
-      className={`z-40 fixed rounded-r-3xl h-[100vh] flex flex-col justify-between bg-transparent`}
+      className={`z-40 fixed bg-background/50 backdrop-blur-2xl rounded-r-3xl h-[100vh] flex flex-col justify-between`}
       initial={{ width: isCollapsed ? '3.5rem' : '15rem' }}
       animate={{ width: isCollapsed ? '15rem' : '3.5rem' }}
       transition={{ duration: 0.3 }}
     >
       <div className={`flex flex-col gap-1 mt-16 mx-2 items-center justify-center`}>
         {menuItens.map((item) => (
-            <div
-                key={item.name}
-                className={`flex flex-row w-full gap-5 px-2 py-2 rounded-xl cursor-pointer ${pathName === item.path && 'dark:bg-neutral-800 light:bg-neutral-500'} dark:hover:bg-neutral-700 light:hover:bg-neutral-500`}
-                onClick={() => {
-                    router.push(isCollapsed && item.path !== undefined ? item.path : pathName)
-                    handleClick(item.name)
-                }}
-            >
-                <motion.div
-                    initial={{ scale: 0 }}
-                    animate={{ rotate: isAnimated[item.name] ? 360 : 0, scale: 1 }}
-                    transition={{
-                        type: 'spring',
-                        stiffness: 260,
-                        damping: 20
-                    }}
-                    >
-                    <Icon path={item.icon} size={1} />
-                </motion.div>
-                <div className='text-nowrap'>
-                  {item.name.split('').map((letter, index) => (
-                    <motion.span
-                      key={index}
-                      initial={{ opacity: isCollapsed ? 0 : 1 }}
-                      animate={{ opacity: isCollapsed ? 1 : 0  }}
-                      transition={{
-                        duration: 0.25,
-                        delay: index / 25,
-                      }}
+          <div
+              key={item.id}
+              className={`flex flex-row w-full gap-5 px-2 py-2 rounded-xl cursor-pointer ${pathName === item.path && 'dark:bg-neutral-800 light:bg-neutral-500'} dark:hover:bg-neutral-700 light:hover:bg-neutral-500`}
+              onClick={() => {
+                  router.push(isCollapsed && item.path !== undefined ? item.path : pathName)
+                  handleClick(item.name)
+              }}
+          >
+              <motion.div
+                  initial={{ scale: 0 }}
+                  animate={{ rotate: isAnimated[item.name] ? 360 : 0, scale: 1 }}
+                  transition={{
+                      type: 'spring',
+                      stiffness: 260,
+                      damping: 20
+                  }}
                   >
-                    {letter}
-                  </motion.span>
-                  ))}
-                </div>
-            </div>
+                  <Icon path={item.icon} size={1} />
+              </motion.div>
+              <div className='text-nowrap'>
+                {item.name.split('').map((letter, index) => (
+                  <motion.span
+                    key={index}
+                    initial={{ opacity: isCollapsed ? 0 : 1 }}
+                    animate={{ opacity: isCollapsed ? 1 : 0  }}
+                    transition={{
+                      duration: 0.25,
+                      delay: index / 25,
+                    }}
+                >
+                  {letter}
+                </motion.span>
+                ))}
+              </div>
+          </div>
         ))}
       </div>
       <div className='flex flex-col gap-5 p-2 mt-10 items-center justify-center'>
-        {sideItens.map((item) => (
+        {sideItens.map((item) => {
+          if (item.id === 'theme-switch' && pathName.includes('video')) return
+          return (
             <div
                 key={item.name}
                 className={`flex flex-row w-full gap-5 px-2 py-2 rounded-xl cursor-pointer ${pathName === item.path && 'dark:bg-neutral-800 light:bg-neutral-500'} dark:hover:bg-neutral-700 light:hover:bg-neutral-500`}
@@ -138,7 +144,8 @@ export function SideBar() {
                   ))}
                 </div>
             </div>
-        ))}
+          )
+      })}
       </div>
     </motion.div>
   )
