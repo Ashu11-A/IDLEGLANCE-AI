c946c99587361ce633e8ca775b4f6b0b2ad8fec8
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-04-04 18:00:15 -0400

    📃 Description similar to Youtube

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -5,6 +5,7 @@ import axios from "axios"
 import { MoreVideoDetails } from 'ytdl-core'
 import { redirect } from 'next/navigation'
 import { ThumbnailsData, YoutubeVideoFormat } from "../../../api/nest/dist/rest/videos/dto/youtube.types"
+import WatchMetadata from "@/components/player/WatchMetadata"
 
 interface VideoPageType {
     searchParams: {
@@ -41,35 +42,12 @@ export interface Video {
 
 export default async function VideoPage({ searchParams: { id } }: VideoPageType) {
   if (id === undefined) redirect('/')
-
-  const { title, description } = await axios.get(`http://localhost:3333/video/${id}`).then((res) => res.data as Video)
   // const stream: Stream[] = {}
-
-  function adicionarLinks(texto: string) {
-    const exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return texto.replace(exp, `<a style="color: rgb(62, 166, 255)" href='$1'>$1</a>`);
-  }
-  function haveLink(text:string) {
-    const exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return exp.test(text)
-  }
-
-  const formatedDes = adicionarLinks(description)
 
   return (
     <div className='flex mx-10 mt-14 min-h-screen'>
       {/* <Player { ...stream[1] } /> */}
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-row">
-          <p className="text-start font-sans text-2xl">{title}</p>
-        </div>
-        <div className="h-[1px] bg-zinc-800 w-full"></div>
-        <div className="bg-zinc-800 rounded-2xl p-5 whitespace-pre-wra">
-          {description.split('\n\n').map((text) => (
-            <pre className="py-3" key={Math.random()} dangerouslySetInnerHTML={{ __html: adicionarLinks(text) }} />
-          ))}
-        </div>
-      </div>
+      <WatchMetadata id={id} />
     </div>
   )
 }
---
+++ src/components/player/WatchMetadata.tsx
@@ -0,0 +1,71 @@
+'use client'
+import { Video } from "@/app/video/page"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+import { Skeleton } from "../ui/skeleton"
+import { useState } from "react"
+
+export default function WatchMetadata({ id }: { id: string}) {
+  const [showMore, setShowMore] = useState(false)  
+  const { data, error } = useQuery({
+    queryKey: [id],
+    queryFn: () => axios.get(`/internal/video/${id}`).then((res) => res.data as Video),
+    refetchOnWindowFocus: false
+  })
+
+  function adicionarLinks(texto: string) {
+    const exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return texto.replace(exp, `<a style="color: rgb(62, 166, 255)" href='$1'>$1</a>`);
+  }
+  function textRefactoring(text: string): string[] {
+    const brokenText: string[] = text.split(' ');
+    const formattedText: string[] = [];
+    let currentSize: number = 0;
+  
+    for (const word of brokenText) {
+      if (currentSize + word.length + 1 <= 120) { // Adicionamos 1 para contar o espaço
+        if (formattedText.length && formattedText[formattedText.length - 1].length + word.length + 1 <= 120) { // Verificamos se a palavra cabe no último elemento
+          formattedText[formattedText.length - 1] += ' ' + word;
+        } else {
+          formattedText.push(word);
+        }
+        currentSize += word.length + 1;
+      } else {
+        break;
+      }
+    }
+    
+    return formattedText;
+  }
+
+  return (
+    <div className="w-full h-full flex flex-col gap-5">
+      <div className="flex flex-row h-6 w-96">
+        {data === undefined
+          ? <Skeleton className="w-full h-full rounded-full" />
+          : <p className="text-start font-semibold text-xl">{data.title.replace('[Lyrics x AMV]', '')}</p>}
+      </div>
+      <div className="h-[1px] bg-zinc-800 w-full"></div>
+      {data === undefined
+        ? <Skeleton className="w-full h-40 rounded-2xl" />
+        : <div className="bg-zinc-800 rounded-2xl p-5 whitespace-pre-wra">
+          {data.description.length > 200 && !showMore ? (
+            <>
+              {textRefactoring(data.description)[0].split('\n\n').map((text) => (
+                <pre className="py-2 text-xs" key={Math.random()} dangerouslySetInnerHTML={{ __html: adicionarLinks(text) }} />
+              ))}
+              <button onClick={() => setShowMore((prev) => !prev)}>Mostrar Mais...</button>
+            </>
+          ) : (
+            <>
+              {data.description.split('\n\n').map((text) => (
+                <pre className="py-2 text-xs" key={Math.random()} dangerouslySetInnerHTML={{ __html: adicionarLinks(text) }} />
+              ))}
+              <button onClick={() => setShowMore((prev) => !prev)}>Mostrar Menos...</button>
+            </>
+          )}
+        </div>
+      }
+    </div>
+  )
+}
